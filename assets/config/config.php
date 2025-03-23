a8b31bb17aece65092ce5ad867b560cdcb9b419a
hamzah <hamzahnuralfalah@gmail.com>
2025-03-23 21:14:55 +0700

    mayor update

--- assets/config/config.php
+++ assets/config/config.php
@@ -3,6 +3,10 @@
 use Hanafalah\ModuleOrganization\Models as ModuleOrganization;
 
 return [
+    'libs' => [
+        'model' => 'Models',
+        'contract' => 'Contracts'
+    ],
     'database' => [
         'models' => [
             'Organization'         => ModuleOrganization\Organization::class,
