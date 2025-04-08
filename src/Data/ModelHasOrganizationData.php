f07d6ee6b39cee7ce6f2555b662f0bbba93a2564
hamzah <hamzahnuralfalah@gmail.com>
2025-04-08 09:15:50 +0700

    mass update of data (DTO)

--- src/Data/ModelHasOrganizationData.php
+++ src/Data/ModelHasOrganizationData.php
@@ -8,25 +8,23 @@ use Spatie\LaravelData\Attributes\MapInputName;
 use Spatie\LaravelData\Attributes\MapName;
 
 class ModelHasOrganizationData extends Data implements DataModelHasOrganizationData{
-    public function __construct(
-        #[MapInputName('id')]
-        #[MapName('id')]
-        public mixed $id = null,
+    #[MapInputName('id')]
+    #[MapName('id')]
+    public mixed $id = null;
     
-        #[MapInputName('reference_type')]
-        #[MapName('reference_type')]
-        public string $reference_type,
+    #[MapInputName('reference_type')]
+    #[MapName('reference_type')]
+    public string $reference_type;
     
-        #[MapInputName('reference_id')]
-        #[MapName('reference_id')]
-        public mixed $reference_id,
+    #[MapInputName('reference_id')]
+    #[MapName('reference_id')]
+    public mixed $reference_id;
 
-        #[MapInputName('organization_type')]
-        #[MapName('organization_type')]
-        public string $organization_type,
-    
-        #[MapInputName('organization_id')]
-        #[MapName('organization_id')]
-        public mixed $organization_id
-    ){}
+    #[MapInputName('organization_type')]
+    #[MapName('organization_type')]
+    public string $organization_type;
+
+    #[MapInputName('organization_id')]
+    #[MapName('organization_id')]
+    public mixed $organization_id;
 }
