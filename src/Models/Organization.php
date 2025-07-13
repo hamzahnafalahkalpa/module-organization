1e518963b125603760e5d8dd20296fc1b6ddbfdb
Hamzah <hamzahnafalah@gmail.com>
2025-07-13 12:27:37 +0700

    update modules

--- src/Models/Organization.php
+++ src/Models/Organization.php
@@ -6,10 +6,20 @@ use Hanafalah\ModuleOrganization\Resources\Organization\{ViewOrganization,ShowOr
 use Hanafalah\LaravelSupport\Concerns\Support\HasPhone;
 use Hanafalah\LaravelSupport\Models\Unicode\Unicode;
 use Hanafalah\ModuleRegional\Concerns\HasAddress;
+use Illuminate\Support\Str;
 
 class Organization extends Unicode
 {
     use HasAddress, HasPhone;
+
+    protected static function booted(): void{
+        parent::booted();
+        static::creating(function ($query) {
+            $morph = $query->getMorphClass();
+            $query->{Str::snake($morph).'_code'} = static::hasEncoding(Str::upper(Str::snake($morph)));
+            $query->flag ??= $morph;
+        });
+    }
 
     public function viewUsingRelation(): array{
         return $this->mergeArray(['parent'],parent::viewUsingRelation());
--- src/Resources/Organization/ViewOrganization.php
+++ src/Resources/Organization/ViewOrganization.php
@@ -3,12 +3,15 @@
 namespace Hanafalah\ModuleOrganization\Resources\Organization;
 
 use Hanafalah\LaravelSupport\Resources\Unicode\ViewUnicode;
+use Illuminate\Support\Str;
 
 class ViewOrganization extends ViewUnicode
 {
   public function toArray(\Illuminate\Http\Request $request): array
   {
+    $code = Str::snake($this->flag)."_code";
     $arr = [
+      $code => $this->{$code},
       "phone_company" => $this->phone_company,
       "email_company" => $this->email_company
     ];
